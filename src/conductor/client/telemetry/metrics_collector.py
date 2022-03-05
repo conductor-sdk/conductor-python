8b335601282166e9e7ab6009dd6c735d3c5c4c9d
gardusig <gustavo.gardusi@gmail.com>
2022-03-05 03:41:38 -0300

    Add implementation for all metrics counters

--- src/conductor/client/telemetry/metrics_collector.py
+++ src/conductor/client/telemetry/metrics_collector.py
@@ -6,7 +6,7 @@ from prometheus_client import CollectorRegistry
 from prometheus_client import Counter
 from prometheus_client import Gauge
 from prometheus_client.multiprocess import MultiProcessCollector
-from typing import List
+from typing import Dict, List
 import os
 
 
@@ -20,78 +20,91 @@ class MetricsCollector:
         MultiProcessCollector(self.registry)
 
     def increment_task_poll_counter(self, task_type: str) -> None:
-        counter = self.__get_counter(
+        self.__increment_counter(
             name=MetricName.TASK_POLL,
             documentation=MetricDocumentation.TASK_POLL,
-            labelnames=[
-                MetricLabel.TASK_TYPE
-            ]
+            labels={
+                MetricLabel.TASK_TYPE: task_type
+            }
         )
-        self.__increment_counter(counter, [task_type])
 
     def increment_task_execution_queue_full_counter(self, task_type: str) -> None:
-        counter = self.__get_counter(
+        self.__increment_counter(
             name=MetricName.TASK_EXECUTION_QUEUE_FULL,
             documentation=MetricDocumentation.TASK_EXECUTION_QUEUE_FULL,
-            labelnames=[
-                MetricLabel.TASK_TYPE
-            ]
+            labels={
+                MetricLabel.TASK_TYPE: task_type
+            }
         )
-        self.__increment_counter(counter, [task_type])
 
-    # public static void incrementUncaughtExceptionCount() {
-    #     incrementCount(THREAD_UNCAUGHT_EXCEPTION);
-    # }
+    def increment_uncaught_exception_counter(self):
+        self.__increment_counter(
+            name=MetricName.THREAD_UNCAUGHT_EXCEPTION,
+            documentation=MetricDocumentation.THREAD_UNCAUGHT_EXCEPTION,
+            labels={}
+        )
 
     def increment_task_poll_error_counter(self, task_type: str, exception: Exception) -> None:
-        counter = self.__get_counter(
+        self.__increment_counter(
             name=MetricName.TASK_POLL_ERROR,
             documentation=MetricDocumentation.TASK_POLL_ERROR,
-            labelnames=[
-                MetricLabel.TASK_TYPE,
-                MetricLabel.EXCEPTION
-            ]
+            labels={
+                MetricLabel.TASK_TYPE: task_type,
+                MetricLabel.EXCEPTION: str(exception)
+            }
         )
-        self.__increment_counter(counter, [task_type, str(exception)])
 
     def increment_task_paused_counter(self, task_type: str) -> None:
-        counter = self.__get_counter(
+        self.__increment_counter(
             name=MetricName.TASK_PAUSED,
             documentation=MetricDocumentation.TASK_PAUSED,
-            labelnames=[
-                MetricLabel.TASK_TYPE
-            ]
+            labels={
+                MetricLabel.TASK_TYPE: task_type
+            }
         )
-        self.__increment_counter(counter, [task_type])
 
-    # public static void incrementTaskExecutionErrorCount(String taskType, Throwable e) {
-    #     incrementCount(
-    #             TASK_EXECUTE_ERROR, TASK_TYPE, taskType, EXCEPTION, e.getClass().getSimpleName());
-    # }
+    def increment_task_execution_error_counter(self, task_type: str, exception: Exception) -> None:
+        self.__increment_counter(
+            name=MetricName.TASK_EXECUTE_ERROR,
+            documentation=MetricDocumentation.TASK_EXECUTE_ERROR,
+            labels={
+                MetricLabel.TASK_TYPE: task_type,
+                MetricLabel.EXCEPTION: str(exception)
+            }
+        )
 
     def increment_task_ack_failed_counter(self, task_type: str) -> None:
-        counter = self.__get_counter(
+        self.__increment_counter(
             name=MetricName.TASK_ACK_FAILED,
             documentation=MetricDocumentation.TASK_ACK_FAILED,
-            labelnames=[
-                MetricLabel.TASK_TYPE
-            ]
+            labels={
+                MetricLabel.TASK_TYPE: task_type
+            }
         )
-        self.__increment_counter(counter, [task_type])
 
-    # public static void incrementTaskAckErrorCount(String taskType, Exception e) {
-    #     incrementCount(
-    #             TASK_ACK_ERROR, TASK_TYPE, taskType, EXCEPTION, e.getClass().getSimpleName());
-    # }
+    def increment_task_ack_error_counter(self, task_type: str, exception: Exception) -> None:
+        self.__increment_counter(
+            name=MetricName.TASK_ACK_ERROR,
+            documentation=MetricDocumentation.TASK_ACK_ERROR,
+            labels={
+                MetricLabel.TASK_TYPE: task_type,
+                MetricLabel.EXCEPTION: str(exception)
+            }
+        )
 
     # public static void recordTaskResultPayloadSize(String taskType, long payloadSize) {
     #     getGauge(TASK_RESULT_SIZE, TASK_TYPE, taskType).getAndSet(payloadSize);
     # }
 
-    # public static void incrementTaskUpdateErrorCount(String taskType, Throwable t) {
-    #     incrementCount(
-    #             TASK_UPDATE_ERROR, TASK_TYPE, taskType, EXCEPTION, t.getClass().getSimpleName());
-    # }
+    def increment_task_update_error_count(self, task_type: str, exception: Exception) -> None:
+        self.__increment_counter(
+            name=MetricName.TASK_UPDATE_ERROR,
+            documentation=MetricDocumentation.TASK_UPDATE_ERROR,
+            labels={
+                MetricLabel.TASK_TYPE: task_type,
+                MetricLabel.EXCEPTION: str(exception)
+            }
+        )
 
     # public static void recordWorkflowInputPayloadSize(
     #         String workflowType, String version, long payloadSize) {
@@ -99,29 +112,39 @@ class MetricsCollector:
     #             .getAndSet(payloadSize);
     # }
 
-    # public static void incrementExternalPayloadUsedCount(
-    #         String name, String operation, String payloadType) {
-    #     incrementCount(
-    #             EXTERNAL_PAYLOAD_USED,
-    #             ENTITY_NAME,
-    #             name,
-    #             OPERATION,
-    #             operation,
-    #             PAYLOAD_TYPE,
-    #             payloadType);
-    # }
+    def increment_external_payload_used_counter(self, entity_name: str, operation: str, payload_type: str) -> None:
+        self.__increment_counter(
+            name=MetricName.EXTERNAL_PAYLOAD_USED,
+            documentation=MetricDocumentation.EXTERNAL_PAYLOAD_USED,
+            labels={
+                MetricLabel.ENTITY_NAME: entity_name,
+                MetricLabel.OPERATION: operation,
+                MetricLabel.PAYLOAD_TYPE: payload_type
+            }
+        )
 
-    # public static void incrementWorkflowStartErrorCount(String workflowType, Throwable t) {
-    #     incrementCount(
-    #             WORKFLOW_START_ERROR,
-    #             WORKFLOW_TYPE,
-    #             workflowType,
-    #             EXCEPTION,
-    #             t.getClass().getSimpleName());
-    # }
+    def increment_workflow_start_error_counter(self, workflow_type: str, exception: Exception) -> None:
+        self.__increment_counter(
+            name=MetricName.WORKFLOW_START_ERROR,
+            documentation=MetricDocumentation.WORKFLOW_START_ERROR,
+            labels={
+                MetricLabel.WORKFLOW_TYPE: workflow_type,
+                MetricLabel.EXCEPTION: str(exception)
+            }
+        )
 
-    def __increment_counter(self, counter: Counter, label_values: List[MetricLabel]) -> None:
-        counter.labels(*label_values).inc()
+    def __increment_counter(
+        self,
+        name: MetricName,
+        documentation: MetricDocumentation,
+        labels=Dict[MetricLabel, str]
+    ) -> None:
+        counter = self.__get_counter(
+            name=name,
+            documentation=documentation,
+            labelnames=labels.keys()
+        )
+        counter.labels(*labels.values()).inc()
 
     def __get_counter(
         self,
