9c20262ff4387587d9adda7f1210fff649398fa6
gardusig <gustavo.gardusi@gmail.com>
2022-03-08 15:13:36 -0300

    Update unit tests with metrics provider process fixes

--- src/conductor/client/automator/task_handler.py
+++ src/conductor/client/automator/task_handler.py
@@ -2,9 +2,9 @@ from conductor.client.automator.task_runner import TaskRunner
 from conductor.client.configuration.configuration import Configuration
 from conductor.client.telemetry.metrics_provider import provide_metrics
 from conductor.client.worker.worker_interface import WorkerInterface
+from multiprocessing import Process
 from typing import List
 import logging
-import multiprocessing
 
 logger = logging.getLogger(
     Configuration.get_logging_formatted_name(
@@ -14,7 +14,11 @@ logger = logging.getLogger(
 
 
 class TaskHandler:
-    def __init__(self, workers: List[WorkerInterface], configuration: Configuration = None):
+    def __init__(
+            self,
+            workers: List[WorkerInterface],
+            configuration: Configuration = None
+    ):
         if not isinstance(workers, list):
             raise Exception('Invalid worker list')
         self.__create_metrics_provider_process()
@@ -25,7 +29,8 @@ class TaskHandler:
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-        pass
+        self.__stop_metrics_provider_process()
+        self.__stop_task_runner_processes()
 
     def start_processes(self) -> None:
         self.__start_metrics_provider_process()
@@ -38,7 +43,7 @@ class TaskHandler:
         logger.info('Joined all processes')
 
     def __create_metrics_provider_process(self):
-        self.metrics_provider_process = multiprocessing.Process(
+        self.metrics_provider_process = Process(
             target=provide_metrics
         )
         logger.info('Created MetricsProvider process')
@@ -51,7 +56,7 @@ class TaskHandler:
 
     def __create_task_runner_process(self, worker, configuration):
         task_runner = TaskRunner(worker, configuration)
-        process = multiprocessing.Process(
+        process = Process(
             target=task_runner.run
         )
         self.task_runner_processes.append(process)
@@ -73,3 +78,21 @@ class TaskHandler:
         for task_runner_process in self.task_runner_processes:
             task_runner_process.join()
         logger.info('Joined TaskRunner processes')
+
+    def __stop_metrics_provider_process(self):
+        self.__stop_process(self.metrics_provider_process)
+
+    def __stop_task_runner_processes(self):
+        for task_runner_process in self.task_runner_processes:
+            self.__stop_process(task_runner_process)
+
+    def __stop_process(self, process: Process):
+        if process == None:
+            return
+        try:
+            process.kill()
+            logger.info(f'Killed process: {process}')
+        except Exception as e:
+            logger.debug(f'Failed to kill process: {process}, reason: {e}')
+            self.metrics_provider_process.terminate()
+            logger.info('Terminated process: {process}')
