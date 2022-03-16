21eb94219ad3e0c14baa7c7aced3fb30f15f357b
gardusig <gustavo.gardusi@gmail.com>
2022-03-16 16:11:35 -0300

    Add support example for C++

---
+++ src/conductor/client/worker/sample/cpp/simple_cpp_worker.py
@@ -0,0 +1,25 @@
+from conductor.client.http.models.task import Task
+from conductor.client.http.models.task_result import TaskResult
+from conductor.client.http.models.task_result_status import TaskResultStatus
+from conductor.client.worker.worker_interface import WorkerInterface
+from ctypes import cdll
+
+
+class CppWrapper:
+    def __init__(self, file_path='./lib.so'):
+        self.cpp_lib = cdll.LoadLibrary(file_path)
+
+    def get_sum(self) -> function:
+        return self.cpp_lib.get_sum
+
+
+class SimpleCppWorker(WorkerInterface):
+    cpp_wrapper = CppWrapper()
+
+    def execute(self, task: Task) -> TaskResult:
+        task_result = self.get_task_result_from_task(task)
+        task_result.add_output_data(
+            'sum', self.cpp_wrapper.get_sum(1, 2)
+        )
+        task_result.status = TaskResultStatus.COMPLETED
+        return task_result
