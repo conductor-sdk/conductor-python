67deb2aec352034cb2e478e7492d61a78fa685b9
gardusig <gustavo.gardusi@gmail.com>
2022-06-14 20:59:55 -0300

    Fixed task def

--- src/conductor/client/workflow/task/task.py
+++ src/conductor/client/workflow/task/task.py
@@ -19,7 +19,7 @@ class TaskInterface(abc.ABC):
         self._task_type = task_type
         self._description = ''
         self._optional = False
-        self._input = {}
+        self._input_parameters = {}
 
     def name(self, name: str) -> TaskInterface:
         if not isinstance(name, str):
@@ -36,16 +36,21 @@ class TaskInterface(abc.ABC):
     def input(self, input: Dict[str, Any]) -> TaskInterface:
         if not isinstance(input, dict):
             raise Exception('invalid type')
-        self._input = input
+        self._input_parameters = input
+        return self
+
+    def optional(self, optional: bool) -> TaskInterface:
+        if not isinstance(optional, bool):
+            raise Exception('invalid type')
+        self._optional = optional
         return self
 
     def to_workflow_task(self) -> WorkflowTask:
         return WorkflowTask(
-            name=self.name,
-            task_reference_name=self.task_reference_name,
-            workflow_task_type=self.task_type,
-            description=self.description,
-            input_parameters=self.input,
-            start_delay=self.start_delay,
-            optional=self.optional,
+            name=self._name,
+            task_reference_name=self._task_reference_name,
+            workflow_task_type=self._task_type,
+            description=self._description,
+            input_parameters=self._input_parameters,
+            optional=self._optional,
         )
