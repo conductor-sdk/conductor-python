43dc997b64a3962a7164e390e9ea635c771c35ce
gardusig <gustavo.gardusi@gmail.com>
2022-06-14 20:06:34 -0300

    Fix parameter for simple task def

--- src/conductor/client/workflow/task/simple_task.py
+++ src/conductor/client/workflow/task/simple_task.py
@@ -6,4 +6,4 @@ from task import TaskInterface
 class SimpleTask(TaskInterface):
     def __init__(self, task_def_name: str, task_reference_name: str) -> SimpleTask:
         super().__init__(task_reference_name, TaskType.SIMPLE)
-        self.name = task_def_name
+        self._name = task_def_name
