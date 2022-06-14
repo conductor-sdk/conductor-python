fb408cd011ba03a5b6aa3db2bdeec7344e2ab2e4
gardusig <gustavo.gardusi@gmail.com>
2022-06-14 20:16:44 -0300

    Added wait task def support

---
+++ src/conductor/client/workflow/task/wait_task.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+from conductor.client.workflow.task.task_type import TaskType
+from task import TaskInterface
+
+
+class WaitTask(TaskInterface):
+    def __init__(self, task_ref_name: str) -> WaitTask:
+        super().__init__(task_ref_name, TaskType.WAIT)
+
+
+class WaitForDurationTask(WaitTask):
+    def __init__(self, task_ref_name: str, duration_time_seconds: int) -> WaitForDurationTask:
+        super().__init__(task_ref_name)
+        self._input = {
+            "duration": str(duration_time_seconds)
+        }
+
+
+class WaitUntilTask(WaitTask):
+    def __init__(self, task_ref_name: str, date_time: str) -> WaitForDurationTask:
+        super().__init__(task_ref_name)
+        self._input = {
+            "until": date_time
+        }
