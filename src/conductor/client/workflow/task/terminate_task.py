5a3dc54543058b50057e16194dfa9e5fca3bc6cf
gardusig <gustavo.gardusi@gmail.com>
2022-06-14 20:11:12 -0300

    Added support for terminate task def

---
+++ src/conductor/client/workflow/task/terminate_task.py
@@ -0,0 +1,14 @@
+from __future__ import annotations
+from conductor.client.workflow.task.task_type import TaskType
+from src.conductor.client.workflow.task.http_input import HttpInput
+from src.conductor.client.workflow.task.workflow_status import WorkflowStatus
+from task import TaskInterface
+
+
+class TerminateTask(TaskInterface):
+    def __init__(self, task_ref_name: str, status: WorkflowStatus, termination_reason: str) -> TerminateTask:
+        super().__init__(task_ref_name, TaskType.TERMINATE)
+        self._input = {
+            "terminationStatus": status,
+            "terminationReason": termination_reason
+        }
