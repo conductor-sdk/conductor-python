3fc87279fc75993b867d63d1acd15fc812456c13
gardusig <gustavo.gardusi@gmail.com>
2022-06-14 23:43:02 -0300

    Added fork join task def

---
+++ src/conductor/client/workflow/task/fork_join.py
@@ -0,0 +1,29 @@
+from __future__ import annotations
+from typing import List
+from conductor.client.workflow.task.task import TaskInterface
+from conductor.client.workflow.task.task_type import TaskType
+from conductor.client.workflow.task.join_task import JoinTask
+from conductor.client.http.models.workflow_task import WorkflowTask
+
+
+class ForkedTask(TaskInterface):
+    _forked_tasks: List[List[TaskInterface]]
+    _join_task: JoinTask
+
+    def __init__(self, task_ref_name: str, forked_tasks: List[TaskInterface]) -> ForkedTask:
+        super().__init__(task_ref_name, TaskType.INLINE)
+        self._forked_tasks = forked_tasks
+        self._join_task = JoinTask(task_ref_name + '_join')
+
+    def to_workflow_task(self) -> WorkflowTask:
+        workflow = super().to_workflow_task()
+        workflow.fork_tasks = []
+        for inner_forked_tasks in self._forked_tasks:
+            converted_inner_forked_tasks = []
+            for inner_forked_task in inner_forked_tasks:
+                converted_inner_forked_tasks.append(
+                    inner_forked_task.to_workflow_task()
+                )
+            workflow.fork_tasks.append(converted_inner_forked_tasks)
+        workflow.join_on = self._join_task.to_workflow_task()
+        return workflow
