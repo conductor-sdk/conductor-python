2610d030f4da44f489c61dd692c1ef50368299d8
gardusig <gustavo.gardusi@gmail.com>
2022-06-14 22:12:37 -0300

    Added start workflow task def

---
+++ src/conductor/client/workflow/task/start_workflow_task.py
@@ -0,0 +1,17 @@
+from __future__ import annotations
+from conductor.client.workflow.task.task_type import TaskType
+from task import TaskInterface
+from conductor.client.http.models.start_workflow_request import StartWorkflowRequest
+
+
+class StartWorkflowTask(TaskInterface):
+    def __init__(self, task_ref_name: str, workflow_name: str, version: int = None, start_workflow_request: StartWorkflowRequest = None) -> StartWorkflowTask:
+        super().__init__(task_ref_name, TaskType.START_WORKFLOW)
+        self._input_parameters = {
+            "startWorkflow": {
+                "name":          workflow_name,
+                "version":       version,
+                "input":         start_workflow_request.input,
+                "correlationId": start_workflow_request.correlation_id,
+            },
+        }
