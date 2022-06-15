f8a19926c372e7d7a95db8ccbf60a03e3f768eb0
gardusig <gustavo.gardusi@gmail.com>
2022-06-14 22:29:13 -0300

    Added kafka publish task def

---
+++ src/conductor/client/workflow/task/kafka_publish.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+from conductor.client.workflow.task.task_type import TaskType
+from src.conductor.client.workflow.task.http_input import HttpInput
+from src.conductor.client.workflow.task.kafka_publish_input import KafkaPublishInput
+from task import TaskInterface
+
+
+class KafkaPublishTask(TaskInterface):
+    def __init__(self, task_ref_name: str, kafka_publish_input: KafkaPublishInput = None) -> KafkaPublishTask:
+        super().__init__(task_ref_name, TaskType.KAFKA_PUBLISH)
+        self._input = {
+            "kafka_request": kafka_publish_input
+        }
