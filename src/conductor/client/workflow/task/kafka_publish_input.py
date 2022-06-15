2fb23044166ad08782ae781e6c5765c64e9f593d
gardusig <gustavo.gardusi@gmail.com>
2022-06-14 22:25:59 -0300

    Added kafka publisk task def input

---
+++ src/conductor/client/workflow/task/kafka_publish_input.py
@@ -0,0 +1,33 @@
+from __future__ import annotations
+from typing import Any, Dict
+
+
+class KafkaPublishInput:
+    _bootstrap_servers: str
+    _key: str
+    _key_serializer: str
+    _value: str
+    _request_timeout_ms: str
+    _max_block_ms: str
+    _headers: Dict[str, Any]
+    _topic: str
+
+    # TODO add json serializer param names
+
+    def __init__(self,
+                 bootstrap_servers: str = None,
+                 key: str = None,
+                 key_serializer: str = None,
+                 value: str = None,
+                 request_timeout_ms: str = None,
+                 max_block_ms: str = None,
+                 headers: Dict[str, Any] = None,
+                 topic: str = None) -> KafkaPublishInput:
+        self._bootstrap_servers = bootstrap_servers
+        self._key = key
+        self._key_serializer = key_serializer
+        self._value = value
+        self._request_timeout_ms = request_timeout_ms
+        self._max_block_ms = max_block_ms
+        self. _headers = headers
+        self._topic = topic
