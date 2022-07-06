54840384526be5b81d328b64118c54af0c2c1377
gardusig <gustavo.gardusi@gmail.com>
2022-07-06 01:33:15 -0300

    Added more APIs to workflow executor

--- src/conductor/client/workflow/executor/workflow_executor.py
+++ src/conductor/client/workflow/executor/workflow_executor.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from typing import Any, Dict, List
 from conductor.client.configuration.configuration import Configuration
 from conductor.client.http.api_client import ApiClient
 from conductor.client.http.api.metadata_resource_api import MetadataResourceApi
@@ -54,7 +55,6 @@ class WorkflowExecutor:
     def get_workflow_status(self, workflow_id: str, include_output: bool, include_variables: bool) -> WorkflowStatus:
         """Gets the workflow by workflow id
 
-        :param async_req bool
         :param str workflow_id: (required)
         :param bool include_output:
         :param bool include_variables:
@@ -66,4 +66,171 @@ class WorkflowExecutor:
             workflow_id,
             include_output,
             include_variables,
+        )
+
+    def get_by_correlation_ids(self, workflow_name: str, include_closed: bool, include_tasks: bool, correlation_ids: List[str]) -> Dict[str, List[WorkflowDef]]:
+        """Lists workflows for the given correlation id list
+
+        :param list[str] body: (required)
+        :param str name: (required)
+        :param bool include_closed:
+        :param bool include_tasks:
+        :return: dict(str, list[Workflow])
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        return self.workflow_client.get_workflows(
+            body=correlation_ids,
+            name=workflow_name,
+            include_closed=include_closed,
+            include_tasks=include_tasks,
+        )
+
+    def pause(self, workflow_id: str) -> None:
+        """Pauses the workflow
+
+        :param str workflow_id: (required)
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        return self.workflow_client.pause_workflow1(
+            workflow_id=workflow_id
+        )
+
+    def resume(self, workflow_id: str) -> None:
+        """Resumes the workflow
+
+        :param str workflow_id: (required)
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        return self.workflow_client.resume_workflow1(
+            workflow_id=workflow_id
+        )
+
+    def terminate(self, workflow_id: str, reason: str) -> None:
+        """Terminate workflow execution
+
+        :param str workflow_id: (required)
+        :param str reason:
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        return self.workflow_client.terminate1(
+            workflow_id=workflow_id,
+            reason=reason
+        )
+
+    def restart(self, workflow_id: str, use_latest_definitions: bool) -> None:
+        """Restarts a completed workflow
+
+        :param str workflow_id: (required)
+        :param bool use_latest_definitions:
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        return self.workflow_client.restart1(
+            workflow_id=workflow_id,
+            use_latest_definitions=use_latest_definitions
+        )
+
+    def retry(self, workflow_id: str, resume_subworkflow_tasks: bool) -> None:
+        """Retries the last failed task  # noqa: E501
+
+        :param str workflow_id: (required)
+        :param bool resume_subworkflow_tasks:
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        return self.workflow_client.retry1(
+            workflow_id=workflow_id,
+            resume_subworkflow_tasks=resume_subworkflow_tasks
+        )
+
+    def rerun(self, rerun_workflow_request: RerunWorkflowRequest, workflow_id: str) -> str:
+        """Reruns the workflow from a specific task
+
+        :param RerunWorkflowRequest body: (required)
+        :param str workflow_id: (required)
+        :return: str
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        return self.workflow_client.rerun(
+            body=rerun_workflow_request,
+            workflow_id=workflow_id,
+        )
+
+    def skip_task_from_workflow(self, workflow_id: str, task_reference_name: str, skip_task_request: SkipTaskRequest) -> None:
+        """Skips a given task from a current running workflow
+
+        :param str workflow_id: (required)
+        :param str task_reference_name: (required)
+        :param SkipTaskRequest body:
+        :return: None
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        return self.workflow_client.skip_task_from_workflow(
+            workflow_id=workflow_id,
+            task_reference_name=task_reference_name,
+            body=skip_task_request,
+        )
+
+    def update_task(self, task_id: str, workflow_id: str, task_output: Dict[str, Any], status: str) -> str:
+        """Update a task
+
+        :param TaskResult body: (required)
+        :return: str
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        task_result = self.__get_task_result(
+            task_id, workflow_id, task_output, status
+        )
+        return self.task_client.update_task(
+            body=task_result,
+        )
+
+    def update_task_by_ref_name(self, task_output: Dict[str, Any], workflow_id: str, task_reference_name: str, status: str) -> str:
+        """Update a task By Ref Name  # noqa: E501
+
+        :param dict(str, object) body: (required)
+        :param str workflow_id: (required)
+        :param str task_ref_name: (required)
+        :param str status: (required)
+        :return: str
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        return self.task_client.update_task1(
+            body=task_output,
+            workflow_id=workflow_id,
+            task_ref_name=task_reference_name,
+            status=status,
+        )
+
+    def get_task(self, task_id: str) -> str:
+        """Get task by Id 
+
+        :param str task_id: (required)
+        :return: Task
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        return self.task_client.get_task(
+            task_id=task_id
+        )
+
+    def __get_task_result(self, task_id: str, workflow_id: str, task_output: Dict[str, Any], status: str) -> TaskResult:
+        return TaskResult(
+            workflow_instance_id=workflow_id,
+            task_id=task_id,
+            output_data=task_output,
+            status=status
         )
