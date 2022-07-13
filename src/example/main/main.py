a5bb076b305dc80632259cb34e2486abc552c51a
gardusig <gustavo.gardusi@gmail.com>
2022-07-13 18:57:04 -0300

    Added example for both type of workers

--- src/example/main/main.py
+++ src/example/main/main.py
@@ -3,15 +3,17 @@ from conductor.client.configuration.configuration import Configuration
 from conductor.client.configuration.settings.authentication_settings import AuthenticationSettings
 from conductor.client.http.models import Task, TaskResult
 from conductor.client.http.models.task_result_status import TaskResultStatus
+from conductor.client.worker.worker import Worker
 from conductor.client.worker.worker_interface import WorkerInterface
+import os
 
 
 class SimplePythonWorker(WorkerInterface):
     def execute(self, task: Task) -> TaskResult:
         task_result = self.get_task_result_from_task(task)
-        task_result.add_output_data('key1', 'value')
-        task_result.add_output_data('key2', 42)
-        task_result.add_output_data('key3', False)
+        task_result.add_output_data('worker_style', 'class')
+        task_result.add_output_data('secret_number', 1234)
+        task_result.add_output_data('is_it_true', False)
         task_result.status = TaskResultStatus.COMPLETED
         return task_result
 
@@ -19,38 +21,34 @@ class SimplePythonWorker(WorkerInterface):
         return 5
 
 
-class WorkerA(WorkerInterface):
-    def execute(self, task: Task) -> TaskResult:
-        task_result = self.get_task_result_from_task(task)
-        task_result.add_output_data('key', 'A')
-        task_result.status = TaskResultStatus.COMPLETED
-        return task_result
-
-
-class WorkerB(WorkerInterface):
-    def execute(self, task: Task) -> TaskResult:
-        task_result = self.get_task_result_from_task(task)
-        task_result.add_output_data('key', 'B')
-        task_result.status = TaskResultStatus.COMPLETED
-        return task_result
+def execute(self, task: Task) -> TaskResult:
+    task_result = self.get_task_result_from_task(task)
+    task_result.add_output_data('worker_style', 'function')
+    task_result.status = TaskResultStatus.COMPLETED
+    return task_result
 
 
 def main():
     # Point to the Conductor Server
     configuration = Configuration(
-        server_api_url='https://play.orkes.io/api',
+        server_api_url="https://pg-staging.orkesconductor.com/api",
         debug=True,
-        authentication_settings=AuthenticationSettings(  # Optional if you are using a server that requires authentication
-            key_id='key',
-            key_secret='secret'
+        authentication_settings=AuthenticationSettings(
+            key_id=os.getenv('KEY'),
+            key_secret=os.getenv('SECRET'),
         )
     )
 
     # Add three workers
     workers = [
-        SimplePythonWorker('python_task_example'),
-        WorkerA('task_a'),
-        WorkerB('task_random_name'),
+        SimplePythonWorker(
+            'python_task_example'
+        ),
+        Worker(
+            task_type='python_task_example',
+            execute_function=execute,
+            poll_interval=0.1,
+        )
     ]
 
     # Start the worker processes and wait for their completion
