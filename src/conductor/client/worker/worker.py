2a2b502686b820351b676de873d2193920559dc0
gardusig <gustavo.gardusi@gmail.com>
2022-07-19 20:36:10 -0300

    Added support for worker details getter, now it will identify the input and output of the user provided function

--- src/conductor/client/worker/worker.py
+++ src/conductor/client/worker/worker.py
@@ -1,20 +1,48 @@
-from typing import Callable
-from typing_extensions import Self
-from conductor.client.worker.worker_interface import WorkerInterface
 from conductor.client.http.models.task import Task
 from conductor.client.http.models.task_result import TaskResult
+from conductor.client.worker.worker_interface import WorkerInterface
+from typing import Any, Callable
+from typing_extensions import Self
+import inspect
 
-ExecuteTaskFunction = Callable[[Task], TaskResult]
+ExecuteTaskFunction = Callable[[Any], Any]
+
+
+def is_callable_input_parameter_a_task(execute_function: ExecuteTaskFunction) -> bool:
+    parameters = inspect.signature(execute_function).parameters
+    if len(parameters) != 1:
+        return False
+    parameter = parameters[list(parameters.keys())[0]]
+    return parameter.annotation == Task
+
+
+def is_callable_return_value_a_task_result(execute_function: ExecuteTaskFunction) -> bool:
+    return_annotation = inspect.signature(execute_function).return_annotation
+    return return_annotation == TaskResult
 
 
 class Worker(WorkerInterface):
     def __init__(self, task_definition_name: str, execute_function: ExecuteTaskFunction,  poll_interval: float) -> Self:
         super().__init__(task_definition_name)
-        self.execute_function = execute_function
         self.poll_interval = poll_interval
+        self.execute_function = execute_function
 
     def execute(self, task: Task) -> TaskResult:
         return self.execute_function(task)
 
     def get_polling_interval_in_seconds(self) -> float:
         return self.poll_interval
+
+    @property
+    def execute_function(self) -> ExecuteTaskFunction:
+        return self._execute_function
+
+    @execute_function.setter
+    def execute_function(self, execute_function: ExecuteTaskFunction) -> None:
+        self._execute_function = execute_function
+        self._is_execute_function_input_parameter_a_task = is_callable_input_parameter_a_task(
+            execute_function
+        )
+        self._is_execute_function_return_value_a_task_result = is_callable_return_value_a_task_result(
+            execute_function
+        )
