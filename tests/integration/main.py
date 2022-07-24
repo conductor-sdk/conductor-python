bb1111efe35acb376fc4b20481499ecbdfd7612f
gardusig <gustavo.gardusi@gmail.com>
2022-07-24 16:27:51 -0300

    Added integration tests server envs example to validate

--- tests/integration/main.py
+++ tests/integration/main.py
@@ -4,14 +4,18 @@ from conductor.client.workflow.executor.workflow_executor import WorkflowExecuto
 from test_workflow_definition import test_kitchensink_workflow_registration
 import os
 
+KEY_ID = os.getenv('PYTHON_INTEGRATION_TESTS_SERVER_KEY_ID')
+KEY_SECRET = os.getenv('PYTHON_INTEGRATION_TESTS_SERVER_KEY_SECRET')
+SERVER_API_URL = os.getenv('PYTHON_INTEGRATION_TESTS_SERVER_API_URL')
+
 
 def generate_configuration():
     return Configuration(
-        server_api_url="https://pg-staging.orkesconductor.com/api",
+        server_api_url=SERVER_API_URL,
         debug=True,
         authentication_settings=AuthenticationSettings(
-            key_id=os.getenv('KE'),
-            key_secret=os.getenv('SECRET'),
+            key_id=KEY_ID,
+            key_secret=KEY_SECRET,
         )
     )
 
