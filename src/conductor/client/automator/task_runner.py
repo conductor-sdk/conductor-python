d955171eb51d4774a539fff2df9995501d422776
gardusig <gustavo.gardusi@gmail.com>
2022-08-06 09:39:52 +0100

    backup code

--- src/conductor/client/automator/task_runner.py
+++ src/conductor/client/automator/task_runner.py
@@ -4,8 +4,10 @@ from conductor.client.http.api_client import ApiClient
 from conductor.client.http.api.task_resource_api import TaskResourceApi
 from conductor.client.http.models.task import Task
 from conductor.client.http.models.task_result import TaskResult
+from conductor.client.http.models.task_result_status import TaskResultStatus
 from conductor.client.telemetry.metrics_collector import MetricsCollector
 from conductor.client.worker.worker_interface import WorkerInterface
+from typing import List
 import logging
 import sys
 import time
@@ -16,6 +18,206 @@ logger = logging.getLogger(
         __name__
     )
 )
+
+
+def run(
+    configuration: Configuration = None,
+) -> None:
+    if configuration != None:
+        configuration.apply_logging_config()
+    while True:
+        run_once()
+
+
+def run_once(
+    configuration: Configuration,
+    task_name: str,
+    poll_interval_in_seconds: float,
+    execute_function,
+    worker_id: str = None,
+    domain: str = None,
+    metrics_collector: MetricsCollector = None,
+) -> None:
+    # TODO discover amount of available workers
+    available_workers = 1
+    tasks = get_tasks_in_batches(
+        configuration=configuration,
+        task_name=task_name,
+        batch_size=available_workers,
+        timeout=poll_interval_in_seconds,
+        worker_id=worker_id,
+        domain=domain
+    )
+    if tasks == None or len(tasks) == 0:
+        wait_for_polling_interval(
+            poll_interval_in_seconds=poll_interval_in_seconds,
+        )
+        return
+    for task in tasks:
+        task_result = execute_task(
+            task=task,
+            execute_function=execute_function,
+            worker_id=worker_id,
+            metrics_collector=metrics_collector,
+        )
+        update_task(
+            configuration=configuration,
+            task_name=task_name,
+            task_result=task_result,
+            metrics_collector=metrics_collector,
+        )
+
+
+def wait_for_polling_interval(poll_interval_in_seconds: float) -> None:
+    logger.debug(f'Sleep for {poll_interval_in_seconds} seconds')
+    time.sleep(poll_interval_in_seconds)
+
+
+def get_task_resource_api(configuration: Configuration) -> TaskResourceApi:
+    return TaskResourceApi(
+        ApiClient(
+            configuration
+        )
+    )
+
+
+def update_task(
+    configuration: Configuration,
+    task_name: str,
+    task_result: TaskResult,
+    metrics_collector: MetricsCollector = None,
+) -> None:
+    logger.debug(
+        'Updating task, id: {task_id}, workflow_instance_id: {workflow_instance_id}, task_definition_name: {task_definition_name}'.format(
+            task_id=task_result.task_id,
+            workflow_instance_id=task_result.workflow_instance_id,
+            task_definition_name=task_name
+        )
+    )
+    task_resource_api = get_task_resource_api(configuration)
+    try:
+        response = task_resource_api.update_task(
+            body=task_result
+        )
+    except Exception as e:
+        if metrics_collector != None:
+            metrics_collector.increment_task_update_error(
+                task_name, type(e)
+            )
+        logger.info(
+            'Failed to update task, id: {task_id}, workflow_instance_id: {workflow_instance_id}, task_definition_name: {task_definition_name}, reason: {reason}'.format(
+                task_id=task_result.task_id,
+                workflow_instance_id=task_result.workflow_instance_id,
+                task_definition_name=task_name,
+                reason=traceback.format_exc()
+            )
+        )
+        return None
+    logger.debug(
+        'Updated task, id: {task_id}, workflow_instance_id: {workflow_instance_id}, task_definition_name: {task_definition_name}, response: {response}'.format(
+            task_id=task_result.task_id,
+            workflow_instance_id=task_result.workflow_instance_id,
+            task_definition_name=task_name,
+            response=response
+        )
+    )
+    return response
+
+
+def get_tasks_in_batches(
+    configuration: Configuration,
+    task_name: str,
+    batch_size: int,
+    timeout: str,
+    worker_id: str = None,
+    domain: str = None,
+    metrics_collector: MetricsCollector = None,
+) -> List[Task]:
+    kwargs = {
+        'count': batch_size,
+        'timeout': timeout
+    }
+    if worker_id != None:
+        kwargs['workerid'] = worker_id
+    if domain != None:
+        kwargs['domain'] = domain
+    if metrics_collector != None:
+        metrics_collector.increment_task_poll(task_name)
+    task_resource_api = get_task_resource_api(configuration)
+    try:
+        start_time = time.time()
+        tasks = task_resource_api.batch_poll(task_name, *kwargs)
+        finish_time = time.time()
+        time_spent = finish_time - start_time
+        if metrics_collector != None:
+            metrics_collector.record_task_poll_time(task_name, time_spent)
+    except Exception as e:
+        if metrics_collector != None:
+            metrics_collector.increment_task_poll_error(
+                task_name, type(e)
+            )
+        logger.info(
+            f'Failed to poll task for: {task_name}, reason: {traceback.format_exc()}'
+        )
+        return None
+    if tasks != None:
+        logger.debug(
+            'Polled {} task(s) for: {} with worker_id: {}'.format(
+                len(tasks), task_name, worker_id
+            )
+        )
+    return tasks
+
+
+def execute_task(
+    task: Task,
+    execute_function,  # TODO add type guide
+    worker_id: str = None,
+    metrics_collector: MetricsCollector = None,
+) -> TaskResult:
+    logger.debug(
+        'Executing task, id: {}, workflow_instance_id: {}, task_definition_name: {}'.format(
+            task.task_id, task.workflow_instance_id, task.task_def_name
+        )
+    )
+    try:
+        start_time = time.time()
+        task_result = execute_function(task)
+        finish_time = time.time()
+        time_spent = finish_time - start_time
+        if metrics_collector != None:
+            metrics_collector.record_task_execute_time(
+                task.task_def_name, time_spent,
+            )
+            metrics_collector.record_task_result_payload_size(
+                task.task_def_name, sys.getsizeof(task_result),
+            )
+        logger.debug(
+            'Executed task, id: {}, workflow_instance_id: {}, task_definition_name: {}'.format(
+                task.task_id, task.workflow_instance_id, task.task_def_name
+            )
+        )
+    except Exception as e:
+        if metrics_collector != None:
+            metrics_collector.increment_task_execution_error(
+                task.task_id, type(e),
+            )
+        task_result = TaskResult(
+            task_id=task.task_id,
+            workflow_instance_id=task.workflow_instance_id,
+            worker_id=worker_id,
+        )
+        task_result.status = TaskResultStatus.FAILED
+        task_result.reason_for_incompletion = str(e)
+        logger.info(
+            'Failed to execute task, id: {}, workflow_instance_id: {}, task_definition_name: {}, reason: {}'.format(
+                task.task_id,
+                task.workflow_instance_id,
+                task.task_def_name,
+                traceback.format_exc(),
+            )
+        )
+    return task_result
 
 
 class TaskRunner:
@@ -33,158 +235,4 @@ class TaskRunner:
         self.configuration = configuration
         self.metrics_collector = MetricsCollector(
             metrics_settings
-        )
-
-    def run(self) -> None:
-        if self.configuration != None:
-            self.configuration.apply_logging_config()
-        while True:
-            self.run_once()
-
-    def run_once(self) -> None:
-        task = self.__poll_task()
-        if task != None:
-            task_result = self.__execute_task(task)
-            self.__update_task(task_result)
-        self.__wait_for_polling_interval()
-
-    def __poll_task(self) -> Task:
-        task_definition_name = self.worker.get_task_definition_name()
-        self.metrics_collector.increment_task_poll(
-            task_definition_name
-        )
-        logger.debug(f'Polling task for: {task_definition_name}')
-        try:
-            start_time = time.time()
-            domain = self.worker.get_domain()
-            if domain != None:
-                task = self.__get_task_resource_api().poll(
-                    tasktype=task_definition_name,
-                    workerid=self.worker.get_identity(),
-                    domain=self.worker.get_domain(),
-                )
-            else:
-                task = self.__get_task_resource_api().poll(
-                    tasktype=task_definition_name,
-                    workerid=self.worker.get_identity(),
-                )
-            finish_time = time.time()
-            time_spent = finish_time - start_time
-            self.metrics_collector.record_task_poll_time(
-                task_definition_name, time_spent
-            )
-        except Exception as e:
-            self.metrics_collector.increment_task_poll_error(
-                task_definition_name, type(e)
-            )
-            logger.info(
-                f'Failed to poll task for: {task_definition_name}, reason: {traceback.format_exc()}'
-            )
-            return None
-        if task != None:
-            logger.debug(
-                f'Polled task: {task_definition_name}, worker_id: {self.worker.get_identity()}'
-            )
-        return task
-
-    def __execute_task(self, task: Task) -> TaskResult:
-        if not isinstance(task, Task):
-            return None
-        task_definition_name = self.worker.get_task_definition_name()
-        logger.debug(
-            'Executing task, id: {task_id}, workflow_instance_id: {workflow_instance_id}, task_definition_name: {task_definition_name}'.format(
-                task_id=task.task_id,
-                workflow_instance_id=task.workflow_instance_id,
-                task_definition_name=task_definition_name
-            )
-        )
-        try:
-            start_time = time.time()
-            task_result = self.worker.execute(task)
-            finish_time = time.time()
-            time_spent = finish_time - start_time
-            self.metrics_collector.record_task_execute_time(
-                task_definition_name,
-                time_spent
-            )
-            self.metrics_collector.record_task_result_payload_size(
-                task_definition_name,
-                sys.getsizeof(task_result)
-            )
-            logger.debug(
-                'Executed task, id: {task_id}, workflow_instance_id: {workflow_instance_id}, task_definition_name: {task_definition_name}'.format(
-                    task_id=task.task_id,
-                    workflow_instance_id=task.workflow_instance_id,
-                    task_definition_name=task_definition_name
-                )
-            )
-        except Exception as e:
-            self.metrics_collector.increment_task_execution_error(
-                task_definition_name, type(e)
-            )
-            task_result = TaskResult(
-                task_id=task.task_id,
-                workflow_instance_id=task.workflow_instance_id,
-                worker_id=self.worker.get_identity()
-            )
-            task_result.status = 'FAILED'
-            task_result.reason_for_incompletion = str(e)
-            logger.info(
-                'Failed to execute task, id: {task_id}, workflow_instance_id: {workflow_instance_id}, task_definition_name: {task_definition_name}, reason: {reason}'.format(
-                    task_id=task.task_id,
-                    workflow_instance_id=task.workflow_instance_id,
-                    task_definition_name=task_definition_name,
-                    reason=traceback.format_exc()
-                )
-            )
-        return task_result
-
-    def __update_task(self, task_result: TaskResult):
-        if not isinstance(task_result, TaskResult):
-            return None
-        task_definition_name = self.worker.get_task_definition_name()
-        logger.debug(
-            'Updating task, id: {task_id}, workflow_instance_id: {workflow_instance_id}, task_definition_name: {task_definition_name}'.format(
-                task_id=task_result.task_id,
-                workflow_instance_id=task_result.workflow_instance_id,
-                task_definition_name=task_definition_name
-            )
-        )
-        try:
-            response = self.__get_task_resource_api().update_task(
-                body=task_result
-            )
-        except Exception as e:
-            self.metrics_collector.increment_task_update_error(
-                task_definition_name, type(e)
-            )
-            logger.info(
-                'Failed to update task, id: {task_id}, workflow_instance_id: {workflow_instance_id}, task_definition_name: {task_definition_name}, reason: {reason}'.format(
-                    task_id=task_result.task_id,
-                    workflow_instance_id=task_result.workflow_instance_id,
-                    task_definition_name=task_definition_name,
-                    reason=traceback.format_exc()
-                )
-            )
-            return None
-        logger.debug(
-            'Updated task, id: {task_id}, workflow_instance_id: {workflow_instance_id}, task_definition_name: {task_definition_name}, response: {response}'.format(
-                task_id=task_result.task_id,
-                workflow_instance_id=task_result.workflow_instance_id,
-                task_definition_name=task_definition_name,
-                response=response
-            )
-        )
-        return response
-
-    def __wait_for_polling_interval(self) -> None:
-        polling_interval = self.worker.get_polling_interval_in_seconds()
-        logger.debug(f'Sleep for {polling_interval} seconds')
-        time.sleep(polling_interval)
-
-    def __get_task_resource_api(self) -> TaskResourceApi:
-        return TaskResourceApi(
-            ApiClient(
-                configuration=self.configuration
-            )
         )
