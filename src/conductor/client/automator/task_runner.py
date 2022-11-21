27758bebf46b6fffca5373375cc22394cbee74a3
dengguojie <denggj7@163.com>
2022-11-21 16:50:53 +0800

    Add a interface in client before polling a task.
    Add paused() interface to check whether to continue polling a task.

--- src/conductor/client/automator/task_runner.py
+++ src/conductor/client/automator/task_runner.py
@@ -52,6 +52,9 @@ class TaskRunner:
 
     def __poll_task(self) -> Task:
         task_definition_name = self.worker.get_task_definition_name()
+        if self.worker.paused():
+            logger.warning(f'Stop polling task for: {task_definition_name}')
+            return None
         if self.metrics_collector is not None:
             self.metrics_collector.increment_task_poll(
                 task_definition_name
