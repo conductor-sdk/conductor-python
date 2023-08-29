3f58b8d796d9654469f8f54187242d165aea657c
Abhishek Gupta <abhishek.gupta1115@gmail.com>
2023-08-28 23:05:07 -0400

    Pass task name to GetDocument

--- src/conductor/client/workflow/task/get_document.py
+++ src/conductor/client/workflow/task/get_document.py
@@ -6,6 +6,7 @@ from typing_extensions import Self
 class GetDocument(TaskInterface):
     def __init__(self, task_name: str, task_ref_name: str, url: str, media_type: str) -> Self:
         super().__init__(
+            task_name=task_name,
             task_reference_name=task_ref_name,
             task_type=TaskType.GET_DOCUMENT,
             input_parameters={
