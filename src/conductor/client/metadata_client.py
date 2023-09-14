1ae114425916e381af000e80a006a20f6ea2fa1e
Abhishek Gupta <abhishek.gupta1115@gmail.com>
2023-09-14 13:26:25 -0400

    Added Tag Management to MetadataClient

--- src/conductor/client/metadata_client.py
+++ src/conductor/client/metadata_client.py
@@ -1,8 +1,9 @@
 from abc import ABC, abstractmethod
 from typing import Optional, List
 from conductor.client.http.models.workflow_def import WorkflowDef
-from conductor.client.http.models.workflow_tag import WorkflowTag
 from conductor.client.http.models.task_def import TaskDef
+from conductor.client.http.models.tag_object import TagObject
+from conductor.client.http.models.tag_string import TagString
 
 class MetadataClient(ABC):
     @abstractmethod
@@ -14,7 +15,7 @@ class MetadataClient(ABC):
         pass
 
     @abstractmethod
-    def unregisterWorkflowDef(self, workflowId: str, version: int):
+    def unregisterWorkflowDef(self, name: str, version: int):
         pass
 
     @abstractmethod
@@ -44,3 +45,36 @@ class MetadataClient(ABC):
     @abstractmethod
     def getAllTaskDefs(self) -> List[TaskDef]:
         pass
+
+    @abstractmethod
+    def addWorkflowTag(self, tagObj: TagObject, workflowName: str):
+        pass
+
+    @abstractmethod
+    def deleteWorkflowTag(self, tagStr: TagString, workflowName: str):
+        pass
+
+    @abstractmethod
+    def getWorkflowTags(self, workflowName: str) -> List[TagObject]:
+        pass
+
+    @abstractmethod
+    def setWorkflowTags(self, tagObjs: List[TagObject], workflowName: str):
+        pass
+
+    @abstractmethod
+    def addTaskTag(self, tagObj: TagObject, taskName: str):
+        pass
+    
+    @abstractmethod
+    def deleteTaskTag(self, tagStr: TagString, taskName: str):
+        pass
+
+    @abstractmethod
+    def getTaskTags(self, taskName: str) -> List[TagObject]:
+        pass
+        
+    @abstractmethod
+    def setTaskTags(self, tagObjs: List[TagObject], taskName: str):
+        pass
+            
