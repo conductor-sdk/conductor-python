731ad78d113294c131edc775a094f1f6f9c84bb4
Abhishek Gupta <abhishek.gupta1115@gmail.com>
2023-11-14 00:09:03 -0500

    Added Workflow Test Integration Test

--- tests/integration/client/orkes/test_orkes_clients.py
+++ tests/integration/client/orkes/test_orkes_clients.py
@@ -1,5 +1,7 @@
+import json
 from shortuuid import uuid
 from conductor.client.configuration.configuration import Configuration
+from conductor.client.http.api_client import ApiClient
 from conductor.client.configuration.settings.authentication_settings import AuthenticationSettings
 from conductor.client.workflow.conductor_workflow import ConductorWorkflow
 from conductor.client.workflow.executor.workflow_executor import WorkflowExecutor
@@ -18,13 +20,17 @@ from conductor.client.http.models.task_result import TaskResult
 from conductor.client.http.models.workflow_def import WorkflowDef
 from conductor.client.http.models.target_ref import TargetRef, TargetType
 from conductor.client.http.models.subject_ref import SubjectRef, SubjectType
+from conductor.client.http.models.workflow_status import WorkflowStatus
 from conductor.client.http.models.task_result_status import TaskResultStatus
 from conductor.client.http.models.save_schedule_request import SaveScheduleRequest
 from conductor.client.http.models.start_workflow_request import StartWorkflowRequest
 from conductor.client.http.models.upsert_user_request import UpsertUserRequest
 from conductor.client.http.models.upsert_group_request import UpsertGroupRequest
 from conductor.client.http.models.create_or_update_application_request import CreateOrUpdateApplicationRequest
+from conductor.client.http.models.workflow_test_request import WorkflowTestRequest
+
 from conductor.client.exceptions.api_error import APIError, APIErrorCode
+
 
 
 SUFFIX = str(uuid())
@@ -35,9 +41,12 @@ SECRET_NAME = 'IntegrationTestSecretClientSec_' + SUFFIX
 APPLICATION_NAME = 'IntegrationTestAuthClientApp_' + SUFFIX
 USER_ID = 'integrationtest_' + SUFFIX[0:5].lower() + "@orkes.io"
 GROUP_ID = 'integrationtest_group_' + SUFFIX[0:5].lower()
+TEST_WF_JSON = 'tests/integration/resources/test_data/calculate_loan_workflow.json'
+TEST_IP_JSON = 'tests/integration/resources/test_data/loan_workflow_input.json'
 
 class TestOrkesClients:
     def __init__(self, configuration: Configuration):
+        self.api_client = ApiClient(configuration)
         self.workflow_executor = WorkflowExecutor(configuration)
         self.metadata_client = OrkesMetadataClient(configuration)
         self.workflow_client = OrkesWorkflowClient(configuration)
@@ -64,6 +73,7 @@ class TestOrkesClients:
         self.test_scheduler_lifecycle(workflowDef)
         self.test_application_lifecycle()
         self.test_user_group_permissions_lifecycle(workflowDef)
+        self.__test_unit_test_workflow()
 
     def test_workflow_lifecycle(self, workflowDef, workflow):
         self.__test_register_workflow_definition(workflowDef)
@@ -196,10 +206,10 @@ class TestOrkesClients:
         
         application = self.authorization_client.getApplication(created_app.id)
         assert application.id == created_app.id
-        
+
         apps = self.authorization_client.listApplications()
         assert True in [app.id == created_app.id for app in apps]
-        
+
         req.name = APPLICATION_NAME + "_updated"
         app_updated = self.authorization_client.updateApplication(req, created_app.id)
         assert app_updated.name == req.name
@@ -217,16 +227,16 @@ class TestOrkesClients:
         self.authorization_client.setApplicationTags(tags, created_app.id)
         fetched_tags = self.authorization_client.getApplicationTags(created_app.id)
         assert len(fetched_tags) == 2
-        
+
         self.authorization_client.deleteApplicationTags(tags, created_app.id)
         fetched_tags = self.authorization_client.getApplicationTags(created_app.id)
         assert len(fetched_tags) == 0
-        
+
         created_access_key = self.authorization_client.createAccessKey(created_app.id)
         access_keys = self.authorization_client.getAccessKeys(created_app.id)
         assert(access_keys[0].id == created_access_key.id)
         assert(access_keys[0].status == AccessKeyStatus.ACTIVE)
-        
+
         access_key = self.authorization_client.toggleAccessKeyStatus(created_app.id, created_access_key.id)        
         assert access_key.status == AccessKeyStatus.INACTIVE
         
@@ -343,6 +353,80 @@ class TestOrkesClients:
         self.metadata_client.updateWorkflowDef(updatedWorkflowDef, True)
         wfDef = self.metadata_client.getWorkflowDef(WORKFLOW_NAME)
         assert len(wfDef.tasks) == 3
+
+    def __test_unit_test_workflow(self):
+        workflowDef = self.__get_workflow_definition(TEST_WF_JSON)
+        assert workflowDef != None
+        
+        testTaskInputs = self.__get_test_inputs(TEST_IP_JSON)
+        assert testTaskInputs != None
+
+        testRequest = WorkflowTestRequest(name=workflowDef.name, workflow_def=workflowDef)
+
+        testRequest.input = {
+            "userEmail": "user@example.com",
+            "loanAmount": 11000,
+        }
+        
+        testRequest.name = workflowDef.name
+        testRequest.version = workflowDef.version
+        testRequest.task_ref_to_mock_output = testTaskInputs
+
+        execution = self.workflow_client.testWorkflow(testRequest)
+        assert execution != None
+        
+        # Ensure workflow is completed successfully
+        assert execution.status == "COMPLETED"
+        
+        # Ensure the inputs were captured correctly
+        assert execution.input["loanAmount"] == testRequest.input["loanAmount"]
+        assert execution.input["userEmail"] == testRequest.input["userEmail"]
+
+        # A total of 7 tasks were executed
+        assert len(execution.tasks) == 7
+
+        fetchUserDetails = execution.tasks[0]
+        getCreditScore = execution.tasks[1]
+        calculateLoanAmount = execution.tasks[2]
+        phoneNumberValidAttempt1 = execution.tasks[4]
+        phoneNumberValidAttempt2 = execution.tasks[5]
+        phoneNumberValidAttempt3 = execution.tasks[6]
+
+        # fetch user details received the correct input from the workflow
+        assert fetchUserDetails.input_data["userEmail"] == testRequest.input["userEmail"]
+
+        userAccountNo = 12345
+        # And that the task produced the right output
+        assert fetchUserDetails.output_data["userAccount"] == userAccountNo
+
+        # get credit score received the right account number from the output of the fetch user details
+        assert getCreditScore.input_data["userAccountNumber"] == userAccountNo
+
+        # The task produced the right output
+        expectedCreditRating = 750
+        assert getCreditScore.output_data["creditRating"] == expectedCreditRating
+
+        # Calculate loan amount gets the right loan amount from workflow input
+        expectedLoanAmount = testRequest.input["loanAmount"]
+        assert calculateLoanAmount.input_data["loanAmount"] == expectedLoanAmount
+        
+        # Calculate loan amount gets the right credit rating from the previous task
+        assert calculateLoanAmount.input_data["creditRating"] == expectedCreditRating
+        
+        authorizedLoanAmount = 10_000
+        assert calculateLoanAmount.output_data["authorizedLoanAmount"] == authorizedLoanAmount
+        
+        assert not phoneNumberValidAttempt1.output_data["valid"]
+        assert not phoneNumberValidAttempt2.output_data["valid"]
+        assert phoneNumberValidAttempt3.output_data["valid"]
+
+        # Finally, lets verify the workflow outputs
+        assert execution.output["accountNumber"] == userAccountNo
+        assert execution.output["creditRating"] == expectedCreditRating
+        assert execution.output["authorizedLoanAmount"] == authorizedLoanAmount
+        
+        # Workflow output takes the latest iteration output of a loopOver task.
+        assert execution.output["phoneNumberValid"]
 
     def __test_unregister_workflow_definition(self):
         self.metadata_client.unregisterWorkflowDef(WORKFLOW_NAME, 1)
@@ -521,3 +605,14 @@ class TestOrkesClients:
         )
         
         assert self.task_client.getQueueSizeForTask(TASK_TYPE) == 0
+
+    def __get_workflow_definition(self, path):
+        f = open(path, "r")
+        workflowJSON = json.loads(f.read())
+        workflowDef = self.api_client.deserialize_class(workflowJSON, "WorkflowDef")
+        return workflowDef
+    
+    def __get_test_inputs(self, path):
+        f = open(path, "r")
+        inputJSON = json.loads(f.read())
+        return inputJSON
