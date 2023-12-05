bfda29216c75c2768b04580ef32980c1ad5cf722
Abhishek Gupta <abhishek.gupta1115@gmail.com>
2023-12-05 13:36:40 -0500

    Revered client/ai pylint changes

--- src/conductor/client/ai/integrations.py
+++ src/conductor/client/ai/integrations.py
@@ -13,37 +13,52 @@ class IntegrationConfig(ABC):
 
 
 class WeviateConfig(IntegrationConfig):
+
     def __init__(self, api_key: str, endpoint: str, classname: str) -> None:
         self.api_key = api_key
         self.endpoint = endpoint
         self.classname = classname
 
     def to_dict(self) -> dict:
-        return {"api_key": self.api_key, "endpoint": self.endpoint}
+        return {
+            'api_key': self.api_key,
+            'endpoint': self.endpoint
+        }
 
 
 class OpenAIConfig(IntegrationConfig):
+
     def __init__(self, api_key: str) -> None:
         self.api_key = api_key
 
     def to_dict(self) -> dict:
-        return {"api_key": self.api_key}
+        return {
+            'api_key': self.api_key
+        }
 
 
 class AzureOpenAIConfig(IntegrationConfig):
+
     def __init__(self, api_key: str, endpoint: str) -> None:
         self.api_key = api_key
         self.endpoint = endpoint
 
     def to_dict(self) -> dict:
-        return {"api_key": self.api_key, "endpoint": self.endpoint}
+        return {
+            'api_key': self.api_key,
+            'endpoint': self.endpoint
+        }
 
 
 class PineconeConfig(IntegrationConfig):
+
     def __init__(self, api_key: str, endpoint: str, classname: str) -> None:
         self.api_key = api_key
         self.endpoint = endpoint
         self.classname = classname
 
     def to_dict(self) -> dict:
-        return {"api_key": self.api_key, "endpoint": self.endpoint}
+        return {
+            'api_key': self.api_key,
+            'endpoint': self.endpoint
+        }
