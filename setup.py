48af2dc07b16d2338126e83c7692bc70b8a94fff
Viren Baraiya <virenx@gmail.com>
2023-12-16 07:53:34 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,10 @@
 import setuptools
 import os
 
+version = os.environ['CONDUCTOR_PYTHON_VERSION']
+if version is None:
+    version = '0.0.0-SNAPSHOT'
+
 setuptools.setup(
-    version=os.environ['CONDUCTOR_PYTHON_VERSION'],
+    version=version,
 )
