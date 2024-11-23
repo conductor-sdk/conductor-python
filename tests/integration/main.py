11e7b1927cd2d356684e8475f07556be38e8f64a
Viren Baraiya <virenx@gmail.com>
2024-11-23 13:01:19 -0800

    clean up configs

--- tests/integration/main.py
+++ tests/integration/main.py
@@ -20,28 +20,7 @@ _logger = logging.getLogger(
 
 
 def generate_configuration():
-    required_envs = {
-        'KEY': 'KEY',
-        'SECRET': 'SECRET',
-        'URL': 'CONDUCTOR_SERVER_URL',
-    }
-    envs = {}
-    for key, env in required_envs.items():
-        value = os.getenv(env)
-        if value is None or value == '':
-            _logger.warning(f'ENV not set - {env}')
-        else:
-            envs[key] = value
-    params = {
-        'server_api_url': envs['URL'],
-        'debug': True,
-    }
-    if 'KEY' in envs and 'SECRET' in envs:
-        params['authentication_settings'] = AuthenticationSettings(
-            key_id=envs['KEY'],
-            key_secret=envs['SECRET']
-        )
-    configuration = Configuration(**params)
+    configuration = Configuration()
     configuration.debug = False
     configuration.apply_logging_config()
 
