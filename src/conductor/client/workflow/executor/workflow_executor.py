f616906687055b3a10c60e751b18b1b79acc0119
orkes-harshil <harshil.raval@orkes.io>
2025-06-16 16:58:05 +0530

    workflow execution with return strategy (#297)

--- src/conductor/client/workflow/executor/workflow_executor.py
+++ src/conductor/client/workflow/executor/workflow_executor.py
@@ -59,6 +59,21 @@ class WorkflowExecutor:
             wait_for_seconds=wait_for_seconds,
         )
 
+    def execute_workflow_with_return_strategy(self, request: StartWorkflowRequest, wait_until_task_ref: str = None,
+                         wait_for_seconds: int = 10, request_id: str = None,
+                         consistency: str = None,
+                         return_strategy: str = None) -> SignalResponse:
+        """Execute a workflow synchronously with optional reactive features"""
+        if request_id is None:
+            request_id = str(uuid.uuid4())
+
+        return self.workflow_client.execute_workflow_with_return_strategy(start_workflow_request=request,
+                                                                          request_id=request_id,
+                                                                          wait_until_task_ref=wait_until_task_ref,
+                                                                          wait_for_seconds=wait_for_seconds,
+                                                                          consistency=consistency,
+                                                                          return_strategy=return_strategy)
+
     def execute(self, name: str, version: Optional[int] = None, workflow_input: Any = {},
                 wait_until_task_ref: str = None, wait_for_seconds: int = 10,
                 request_id: str = None, correlation_id: str = None, domain: str = None) -> WorkflowRun:
